0d777ad9f8358cd459363aaa9051958e75d47e0d
kevinwgutierrez <51988445+kevinwgutierrez@users.noreply.github.com>
2019-10-21 00:05:40 -0700

    Common folder

---
+++ common/dist/yupSchemas/user.d.ts
@@ -0,0 +1,9 @@
+import * as yup from "yup";
+export declare const emailNotLongEnough = "email must be at least 3 characters";
+export declare const passwordNotLongEnough = "password must be at least 3 characters";
+export declare const invalidEmail = "email must be a valid email";
+export declare const registerPasswordValidation: yup.StringSchema<string>;
+export declare const validUserSchema: yup.ObjectSchema<yup.Shape<object, {
+    email: string;
+    password: string;
+}>>;
